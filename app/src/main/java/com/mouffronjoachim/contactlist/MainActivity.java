e42a313c6e58b4f922a0fbce84d1cb8c20a91fd3
perferic <perferic@gmail.com>
2018-10-31 17:10:51 +0100

    Première version de l'appli
    Mise en place des éléments de L'UI, de binding des éléments créés en XML, gestion des Intents, création d'une classe Contact et d'une class Custom ArrayAdapter: version sans ViewHolder et sans RecyclerView

--- app/src/main/java/com/mouffronjoachim/contactlist/MainActivity.java
+++ app/src/main/java/com/mouffronjoachim/contactlist/MainActivity.java
@@ -1,22 +1,143 @@
 package com.mouffronjoachim.contactlist;
-// Gérer les imports de dépendences
+//TODO: Gérer les imports de dépendances
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
+import java.io.Serializable;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    //TODO: Initialiser les éléments
+    Button  selectionButton;
+    ListView contactList;
+    CustomArrayAdapter contactAdapter;
+    ArrayList<Contact> contacts = new ArrayList<Contact>(){};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Créer en java les élemnts: une liste, un adapteur de liste, un bouton
-        // "Binder" la liste de contact
-        // "Binder les bouton de validation
-        // "Binder" les checkbox de la liste
-        // Créer l'écouteur/listener du bouton
-        // Gérer le cas où aucune checkbox n'est sélectionnée
-        // Gérer les checkbox sélectionnés
-        // Créer un Intent vers l'activité ValidationActivity
-        // Créer un payload pour mettre la liste des contacts sélectionnés
+        //TODO: Créer en java les élements: une liste, un adapteur de liste, un bouton
+        contacts.add(new Contact("Quentin","0123456789"));
+        contacts.add(new Contact("Joachim","0123456789"));
+        contacts.add(new Contact("Lawawa","0123456789"));
+        contacts.add(new Contact("Jacques","0123456789"));
+        contacts.add(new Contact("Thomas","0123456789"));
+        //TODO: "Binder le bouton de validation
+        selectionButton = findViewById(R.id.selectionButton);
+        //TODO: "Binder" la liste de contact
+        contactList = findViewById(R.id.contactListFinal);
+        contactAdapter = new CustomArrayAdapter(this, contacts);
+        //TODO: "Binder" les checkbox de la liste
+        final ArrayList<Contact> selectedContacts = new ArrayList<>();
+        contactList.setAdapter(contactAdapter);
+
+        //TODO: Créer l'écouteur/listener du bouton
+        selectionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: Créer un Intent vers l'activité ValidationActivity
+                Intent intent = new Intent(getApplicationContext(), ValidationActivity.class);
+                //TODO: Créer un payload pour mettre la liste des contacts sélectionnés
+                intent.putExtra("contacts", (Serializable)selectedContacts);
+                startActivity(intent);
+            }
+        });
+        //TODO: Créer l'écouteur/listener des checkboxes
+    }
+}
+
+//TODO: Créer une classe Contact
+//On va créer des objets Contact pour remplir la ListView
+class Contact{
+    public boolean active;
+    public String name;
+    public String tel;
+
+    public Contact(String name, String tel){
+        this.name = name;
+        this.tel = tel;
+        this.active = false;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getTel() {
+        return this.tel;
+    }
+    public boolean getActive(){
+        return this.active;
+    }
+
+    public void switchActive() {
+        this.active = !this.active;
+    }
+}
+
+//TODO: Créer un Adapter spécifique pour inclure des Contacts
+//On fait hériter notre adapter pour gérer les objets Contact
+class CustomArrayAdapter extends ArrayAdapter<Contact>{
+
+    private Context customContext;
+    private ArrayList<Contact> contactList;
+
+    //Faire de context non null pour éviter des problèmes d'affichage grace à un décorateur
+    //Spécifier avec un décorateur que la liste est créée avec un layout
+    public CustomArrayAdapter(@NonNull Context context, ArrayList<Contact> list){
+        super(context, 0, list);
+        this.customContext = context;
+        this.contactList = list;
+    }
+
+    @Override
+    public int getCount() {
+        return contactList.size();
+    }
+
+    @Override
+    public Contact getItem(int position) {
+        return contactList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView == null){
+            convertView = LayoutInflater.from(customContext).inflate(R.layout.custom_view_list,parent,false);
+        }
+        final Contact contact = contactList.get(position);
+
+        CheckBox check = convertView.findViewById(R.id.item_checkbox);
+        //TODO: Gérer les checkbox sélectionnés
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contact.switchActive();
+            }
+        });
+
+        TextView name = convertView.findViewById(R.id.itemText);
+        name.setText(contact.getName());
+
+        TextView tel = convertView.findViewById(R.id.itemTel);
+        tel.setText(contact.getTel());
+
+        return convertView;
     }
 }
--- app/src/main/java/com/mouffronjoachim/contactlist/ValidationActivity.java
+++ app/src/main/java/com/mouffronjoachim/contactlist/ValidationActivity.java
@@ -1,19 +1,63 @@
 package com.mouffronjoachim.contactlist;
-// Gérer les dépendences
+//TODO: Gérer les dépendances
+import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class ValidationActivity extends AppCompatActivity {
+    //TODO: Créer les éléments : 2 boutons, une zone de texte/une liste
+    Button sendButton, cancelButton;
+    TextView contactsText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_validation);
-        // Créer les éléments en java : 2 boutons, une zone de texte/une liste
-        // "Binder" les boutons de validation et d'anulation
-        // Gérer les écouteurs/listeners des boutons
-        // Créer un toast annonçant un clic
-        // Créer un Intent ramenant à la MainActivity
-        // Créer une zone de texte pour afficher les contacts sélectionnés
+
+        //TODO: "Binder" les boutons de validation et d'anulation
+        sendButton = findViewById(R.id.sendButton);
+        cancelButton = findViewById(R.id.cancelButton);
+        contactsText = findViewById(R.id.contactsText);
+        Intent intentResponse = getIntent();
+        ArrayList<Contact> sentContacts = (ArrayList<Contact>)intentResponse.getSerializableExtra("contacts");
+        StringBuilder contactMessage = new StringBuilder();
+        for(Contact contact: sentContacts){
+            contactMessage.append(contact);
+        }
+        contactsText.setText(contactMessage);
+
+        //TODO: Créer un toast annonçant un clic
+        final Toast toast = Toast.makeText(getApplicationContext(),"Il y a eu un clic",Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER,0,0);
+
+        //TODO: Gérer les écouteurs/listeners des boutons
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toast.show();
+                Intent intent = new Intent(Intent.ACTION_SEND_MULTIPLE);
+                startActivity(intent);
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               //TODO: Créer un Intent ramenant à la MainActivity
+               Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+               startActivity(intent);
+            }
+        });
+
+
+
     }
 }
