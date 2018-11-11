579fccf7f3b9db8e6a911fa62226886a487c6638
Lawawa <44605950+Lawawa@users.noreply.github.com>
2018-11-12 06:06:15 +1100

    Add files via upload

---
+++ app/src/main/java/com/mouffronjoachim/contactlist/Voeux.java
@@ -0,0 +1,44 @@
+package vu.yawanda.lawawa.yawanda_test;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+
+public class Voeux extends AppCompatActivity {
+    //Variables d'instance
+    public String[] originaltext;
+    public String[] formatedtext;
+    public int id;
+    public boolean selection;
+
+    //Créateur
+
+    public Voeux() {
+    }
+
+    //Méthodes
+
+    public void setOriginaltext(String[] text) {
+        originaltext = text;
+    }
+
+    public void setFormatedtext(String[] text) {
+        formatedtext = text;
+    }
+
+    public void Formattext(String[] prenom) {
+        //Création d'une variable temporaire pour insérer la phrase personnalisée des prénoms choisis
+        String[] temp = new String[prenom.length];
+        for (int i = 0; i < prenom.length; i++) {
+            temp[i] = String.format(formatedtext[id], prenom[i]);
+        }
+        formatedtext = temp;
+    }
+
+    public String[] getOriginaltext() {
+        return originaltext;
+    }
+
+    public String[] getFormatedtext() {
+        return formatedtext;
+    }
+}
